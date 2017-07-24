b679115d450580f454582d88b95d814747d6f822
BubbleM <1870408473@qq.com>
2017-07-24 14:33:50 -0800

    update Vue_course1

--- vue/vue1.js
+++ vue/vue1.js
@@ -1,6 +1,7 @@
 /*
- * course1任务:了解getter和setter hasOwnProperty ES5的defineProperty
+ * course1任务:了解getter和setter hasOwnProperty  ES5的defineProperty
  * */
+
 // 观察者构造函数
 function Observer(data) {
   this.data = data;
@@ -10,41 +11,46 @@ function Observer(data) {
 let p = Observer.prototype;
 
 /*
- * 此函数用于深层次遍历对象的各个属性
- * 采用的是递归的思路
- * 因为我们要为对象的每一个属性绑定getter和setter
- * */
+* 深层次遍历对象的各个属性　
+* */
 p.walk = function (obj) {
-  let val;
+  let val; // 存储属性的值
   for (let key in obj) {
-    /*
-     * 用hasOwnProperty过滤
-     * 由于for...in循环会把对象原型链上的所有可枚举属性都循环出来
-     * 而我们想要的仅仅是这个对象本身拥有的属性
-     * */
-    if (obj.hasOwnProperty(key)) {
+    // console.log(key)
+    // 因为for...in循环会把对象原型链上所有可枚举属性都循环出来　
+    // 而我们仅需要这个对象本身拥有的属性
+    if (obj.hasOwnProperty(key)) {　
       val = obj[key];
-      //这里判断　如果还没有遍历到最底层,继续new Observer
-      if (typeof  val === 'object') {
+      // 这里判断,如果还没有遍历到最底层,继续new Observer
+      if (typeof val === 'object') {
         new Observer(val);
       }
+      console.log(key + '---' + val )
       this.convert(key, val);
     }
   }
 };
 
+/*
+* 添加get和set方法
+* */
 p.convert = function (key, val) {
+  /*
+  * @param this.data 要在this.data上定义属性
+  * @param key 要定义或修改的属性的名称
+  * @param {} 定义或修改属性的描述符
+  * */
   Object.defineProperty(this.data, key, {
-    enumerable: true,
-    configurable: true,
-    get: function () {
+    configurable: true, // 可删除或改变
+    enumerable: false, // 可枚举
+    get: function () { // 函数返回值将被用作属性的值
       console.log('你访问了' + key);
       return val;
     },
-    set: function (newVal) {
+    set: function (newVal) { // 该函数将仅接收参数赋值给该属性的新值
       console.log('你设置了' + key);
-      console.log('新的' + key + '=' + newVal);
-      if (newVal === val) return;
+      console.log('新的' + key + ' = ' + newVal);
+      if (newVal == val) return;
       val = newVal;
     }
   })
@@ -52,11 +58,11 @@ p.convert = function (key, val) {
 
 let data = {
   user: {
-    name: 'bubble',
-    age: "18"
+    name: 'BubbleM',
+    age: 19
   },
   address: {
-    city: "xian"
+    city: 'xian'
   }
 };
 
